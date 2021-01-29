0b60c6e8213cde2b85ffd01d3d89e1b320660527
wyettlei <wyettlei@hotmail.com>
2021-01-29 19:10:03 +0800

    kafka

---
+++ src/basic_sample/config_parser/config_struct.go
@@ -0,0 +1,10 @@
+package config_parser
+
+/**
+ * @author     : wyettLei
+ * @date       : Created in 2021/1/15 14:27
+ * @description: TODO
+ */
+
+type Mongo_Conn struct {
+}
---
+++ src/basic_sample/config_parser/propParser.go
@@ -0,0 +1,7 @@
+package config_parser
+
+/**
+ * @author     : wyettLei
+ * @date       : Created in 2021/1/15 14:24
+ * @description: TODO
+ */
